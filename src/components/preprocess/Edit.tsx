61f36be8c718e86f46df61990be6d6fa750ffa63
andrewass <a.wassum@gmail.com>
2019-07-06 22:44:16 +0200

    adding stylesheet etc

--- src/components/preprocess/Edit.tsx
+++ src/components/preprocess/Edit.tsx
@@ -1,30 +1,22 @@
 import * as React from 'react'
-import { Link } from 'react-router-dom';
-import Instance from './Instance';
+import { Link } from 'react-router-dom'
+import Instance from './Instance'
+import "../../stylesheets/styles.css"
+import { throws } from 'assert';
 
 export default class Edit extends React.Component<any, any>{
 
     constructor(props: any) {
         super(props)
-    }
-
-    getAttributeNames(firstInstance: any) {
-        return firstInstance.attributeNameList.map((attributeName: string) => attributeName + "\t")
+        this.state = {
+            instances : this.props.instances    
+        }
     }
 
     render() {
         return (
             <div>
                 <h3>Edit Page</h3>
-                <div>
-                    <table cellPadding="15">
-                        <tbody>
-                            {this.getAttributeNames(this.props.instances[0])}
-                            {this.props.instances.map((instance: any) =>
-                                <tr> <Instance instanceData={instance} /> </tr>)}
-                        </tbody>
-                    </table>
-                </div>
                 <Link to="/preprocess"><button>Go back</button></Link>
             </div>
         )
